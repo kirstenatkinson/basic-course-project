3d32fcb410188a1d283142fdb2de1f1650d3623b
kirstenatkinson <atk20002@byui.edu>
2025-02-22 18:29:19 -0700

    week 07 updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-angular-app';
-  loadedFeature = 'recipe';
-  onNavigate(feature: string) {
-    this.loadedFeature = feature;
-  }
+
 }
